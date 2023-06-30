9cfb70c12942307371b58bf3a8dfa2a72c3ca445
Chirag Chauhan <chiragchauhan2002.cc@gmail.com>
2023-07-01 00:12:34 +0530

    final commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import News from "./news";
+import Nav from "./nav";
+import {BrowserRouter,Switch,Route,Link} from "react-router-dom"
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <BrowserRouter>
+  <>
+  <Nav />
+    <Route exact path="/Apple"><News newsName="iphone" /></Route>
+    <Route path="/Tesla"><News newsName="tesla" /></Route>
+    <Route path="/Bitcoin"><News newsName="bitcoin" /></Route>
+    <Route path="/nasa"><News newsName="nasa" /></Route>
+    <Route path="/upsc"><News newsName="upsc" /></Route>
+  </>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
